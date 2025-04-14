e5b2e38bb77e803112f80599979b62d0900c00a2
JuanAndres-source <juanandres234t@gmail.com>
2025-04-14 08:32:51 +0200

    Implementation of Characters and api fetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
- import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
- import { provideRouter } from '@angular/router';
- 
- import { routes } from './app.routes';
- import { HttpClient, provideHttpClient } from '@angular/common/http';
- 
- export const appConfig: ApplicationConfig = {
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-   providers: [
-     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideHttpClient()
-     ]
-    };
+import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()
+    ]
+};
--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  imports: [CommonModule
+  ],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.css'
 })
-export class CharactersComponent {
 
+export class CharactersComponent implements OnInit{
+  characters: any[] = [];
+  totalPages: number = 1;
+  selectedCharacter: any = null;
+  isModalOpen: boolean = false;
+  currentPage: number = 1;
+  transformations: any[] = [];
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.getCharacters(this.currentPage, 10).subscribe({
+      next: (data) => {
+        this.characters = data.items || data;
+        this.totalPages = data.meta.totalPages || 1; 
+        console.log(this.characters);
+    },
+    error: (error)=>{
+      console.error('Error fetching characters:', error);
+    }
+  });
+  }
+
+  closeModal():void{
+    this.selectedCharacter = null;
+    this.isModalOpen = false;
+  }
+  openModal(character: any): void{
+    this.apiService.getCharacterById(character.id).subscribe({
+      next: (data) => {
+        this.selectedCharacter = data;
+        this.transformations = data.transformations || []; 
+        console.log(this.selectedCharacter);
+      },
+      error: (error) => {
+        console.error('Error fetching character details:', error);
+      }
+    });
+    this.isModalOpen = true;
+  }
+
+  nextPage(): void {
+    this.currentPage++;
+    this.apiService.getCharacters(this.currentPage, 10).subscribe({
+      next: (data) => {
+        this.characters = data.items || data;
+        console.log(this.characters);
+      },
+      error: (error) => {
+        console.error('Error fetching characters:', error);
+      }
+    });
+}
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.apiService.getCharacters(this.currentPage, 10).subscribe({
+        next: (data) => {
+          this.characters = data.items || data;
+          console.log(this.characters);
+        },
+        error: (error) => {
+          console.error('Error fetching characters:', error);
+        }
+      });
+    }
+  }
 }
